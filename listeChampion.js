b34e1ac1936e30f91095cc7dbba0d6aed95b51fd
Dxvy <o.mist1303@gmail.com>
2022-02-02 14:21:13 +0100

    ajout fichier js et json, changement de la page html liste des champions

--- listeChampion.js
+++ listeChampion.js
@@ -1,3 +1,40 @@
-{
+const divChampion = `
+    <div class="champion">
+        <a href="Champions/__name__/Aatrox.html">
+            <img src="Champions/akali/ChampionList_akali.jpg" alt="champion___name__"/>
+            __name__
+        </a>
+    </div>`
+;
+console.log("js")
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("api/liste.json").then((response) =>
+        response.json().then(fetchApiDone)
+    );
+});
 
-}
+const fetchApiDone = (json) => {
+    const divList = document.getElementById("list");
+    console.log("json: ", json)
+    json.forEach((champion) => {
+        console.log("champion: ", champion)
+        console.log("champion.name: ", champion.name)
+
+        let divChampion = document.createElement("div");
+        divChampion.className = "champion";
+        divList.appendChild(divChampion);
+
+        let aChampion = document.createElement("a");
+        aChampion.textContent = champion.name;
+        aChampion.href = "Champions/"+champion.name+"/"+champion.name+".html"
+        divChampion.appendChild(aChampion);
+
+        let imgChampion = document.createElement("img");
+        imgChampion.src = "Champions/"+champion.name+"/ChampionList_"+champion.name.toLowerCase()+".jpg";
+        imgChampion.alt = "champion_"+champion.name;
+        aChampion.appendChild(imgChampion);
+
+
+    });
+};
+
